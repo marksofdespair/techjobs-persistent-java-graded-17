b3674bc671926f4b1333934177f4b2914f6ed6cf
Dia <142447820+marksofdespair@users.noreply.github.com>
2024-02-12 20:03:53 -0600

    all tests pass, small kinks

--- queries.sql
+++ queries.sql
@@ -1,7 +1,15 @@
 --Part 1
-
+--Id | int
+--employer | varcharname
+--name | varchar
+--skills | varchar
+--employer_id | int
 --Part 2
-
+SELECT name FROM employer WHERE location = "St. Louis City";
 --Part 3
-
+DROP TABLE job;
 --Part 4
+SELECT * FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
